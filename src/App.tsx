52a2ebdd149960aa4113c57e33783e25a2bcb4bd
momo <harlie0108@gmail.com>
2024-04-21 17:57:46 +0900

    test: add App.spec.tsx  test

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect, useState } from 'react';
+// import axios from 'axios';
+import { decrease, increase } from './utils/utils';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [greet, setGreet] = useState('');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setGreet('hello TDD');
+    }, 100);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="card">
+      <div>{count}</div>
+      <button onClick={() => setCount(count => increase(count))}>증가</button>
+      <button onClick={() => setCount(count => decrease(count))}>감소</button>
+      {greet && <p data-testid="greet">{greet}</p>}
+    </div>
   );
 }
 

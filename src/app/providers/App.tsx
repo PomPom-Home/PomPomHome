cd84865a8aca79b2072a2ed0a85f3948b7808af9
momo <harlie0108@gmail.com>
2024-04-29 01:05:12 +0900

    fix: setting fsd

--- src/app/providers/App.tsx
+++ src/app/providers/App.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from 'styled-components';
-import GlobalStyles from './styles/GlobalStyles';
-import { theme } from './styles/Theme';
+import GlobalStyles from '../styles/GlobalStyles';
+import { theme } from '../styles/Theme';
+import { BrowserRouter } from './RouterProvider';
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
+        <BrowserRouter />
         {/* #20240423.syjang, 이 부분에 메인 컴포넌트 추가 필요 */}
       </ThemeProvider>
     </>
